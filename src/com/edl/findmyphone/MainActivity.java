d2db29bd72404027f9f60051db4612f76eaa30c5
Botrix <1486759736@qq.com>
2016-05-11 19:07:12 +0800

    update LocationService

--- src/com/edl/findmyphone/MainActivity.java
+++ src/com/edl/findmyphone/MainActivity.java
@@ -5,13 +5,60 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.edl.findmyphone.service.LocationService;
+
 public class MainActivity extends Activity {
+	private LocationService locationService;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// -----------location config ------------
+		locationService = ((ChatApplication) getApplication()).locationService;
+		locationService.start();
+		//获取locationservice实例，建议应用中只初始化1个location实例，然后使用，可以参考其他示例的activity，都是通过此种方式获取locationservice实例的
+		locationService.registerListener(mListener);
+
 	}
+
+	/***
+	 * Stop location service
+	 */
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		locationService.unregisterListener(mListener); //注销掉监听
+		locationService.stop(); //停止定位服务
+		super.onStop();
+	}
+/*
+	@Override
+	protected void onStart() {
+		super.onStart();
+		// -----------location config ------------
+		locationService = ((LocationApplication) getApplication()).locationService;
+		//获取locationservice实例，建议应用中只初始化1个location实例，然后使用，可以参考其他示例的activity，都是通过此种方式获取locationservice实例的
+		locationService.registerListener(mListener);
+	}*/
+
+	/*****
+	 *      定位结果回调，重写onReceiveLocation方法，可以直接拷贝如下代码到自己工程中修改
+	 */
+	private BDLocationListener mListener = new BDLocationListener() {
+		@Override
+		public void onReceiveLocation(BDLocation location) {
+			// TODO Auto-generated method stub
+			if (null != location && location.getLocType() != BDLocation.TypeServerError) {
+				 System.out.println("手机定位："+location.getLatitude()+"---"+location.getLongitude());
+
+			}
+		}
+	};
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
