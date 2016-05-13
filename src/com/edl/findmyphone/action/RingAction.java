0e3a74b1699fb285969e40dfd8dda37a15ea3c34
Botrix <1486759736@qq.com>
2016-05-13 16:55:25 +0800

    update

---
+++ src/com/edl/findmyphone/action/RingAction.java
@@ -0,0 +1,35 @@
+package com.edl.findmyphone.action;
+
+import android.content.Context;
+
+import java.util.Map;
+
+/**
+ * 发出铃声
+ * @author Administrator
+ *
+ */
+public class RingAction extends Action {
+
+	@Override
+	public String getAction() {
+		return "ring";
+	}
+
+	@Override
+	public boolean doAction(Context context, Map<String, Object> data) {
+		if (data == null) {
+			return false;
+		}
+
+		String receiver = data.get("receiver").toString();
+		String sender = data.get("sender").toString();
+		
+		
+
+		
+       return true;
+
+	}
+
+}
